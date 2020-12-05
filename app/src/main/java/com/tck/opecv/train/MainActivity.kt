a2009e4b20b7468f26d220b7b23d9d8fc61f6a64
tck8888 <l>
2020-12-05 16:31:02 +0800

    初始化项目

---
+++ app/src/main/java/com/tck/opecv/train/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.tck.opecv.train
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.opencv.android.OpenCVLoader
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+        OpenCVLoader.initDebug()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
